b2e6a704aad97a2d91aa8424887f8b3bd87a3e8e
Mark Harmon <neothej1sec@gmail.com>
2022-11-27 06:27:02 -1000

    working on new backend models

--- back/accounts/models.py
+++ back/accounts/models.py
@@ -19,3 +19,18 @@ class Transaction(models.Model):
     date_modified = models.DateTimeField(auto_now=True, null=True, blank=True)
     def __str__(self):
         return self.location[:10]
+    
+class Investment(models.Model):
+    title = models.CharField(max_length=100, null=True)
+    tag = models.CharField(max_length=20, null=True)
+    description = models.TextField(default='')
+    in_price = models.CharField(max_length=100, null=True)
+    in_amt = models.CharField(max_length=100, null=True)
+    pull_min = models.CharField(max_length=100, null=True)
+    pull_cap = models.CharField(max_length=100, null=True)
+    is_complete = models.BooleanField(default=False)
+    out_price = models.CharField(max_length=100, null=True)
+    out_amt = models.CharField(max_length=100, null=True)
+    def __str__(self):
+        return self.tag
+    
